e6b3f57275f5af924dbabb7a3328b54277c3e4d7
nikolascoelho <nikolasgianoglou.coelho-ext@ab-inbev.com>
2023-12-05 10:22:54 -0300

    Creation of Coordinator, CoordinatorViewModel and CoordinatorView

---
+++ Alelo/Coordinator/Coordinator.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinator.swift
+//  Alelo
+//
+//  Created by Nikolas Gianoglou Coelho on 05/12/2023.
+//
+
+import Foundation
+
+enum Coordinator {
+    case home
+}
---
+++ Alelo/Coordinator/CoordinatorView.swift
@@ -0,0 +1,20 @@
+//
+//  CoordinatorView.swift
+//  Alelo
+//
+//  Created by Nikolas Gianoglou Coelho on 05/12/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct CoordinatorView: View {
+    @StateObject var viewModel = CoordinatorViewModel.shared
+    
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+    
+}
