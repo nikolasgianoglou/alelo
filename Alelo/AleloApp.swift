4cecd702446914f92adec452c3698b54ebd30542
nikolascoelho <nikolasgianoglou.coelho-ext@ab-inbev.com>
2023-12-05 10:02:33 -0300

    Creation of initial product

---
+++ Alelo/AleloApp.swift
@@ -0,0 +1,17 @@
+//
+//  AleloApp.swift
+//  Alelo
+//
+//  Created by Nikolas Gianoglou Coelho on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AleloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
